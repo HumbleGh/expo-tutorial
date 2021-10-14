22886cfef0976fc6828de9679d88d96a54e298d4
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2021-10-14 11:55:22 +0000

    changes to screens

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <View>
+        <NavigationContainer>
+          <MainNavigator>
+            <MainNavigator.Screen name="Logn" component={Login} />
+            <MainNavigator.Screen name="Home" component={Home} />
+            <MainNavigator.Screen name="Cart" component={Cart} />
+          </MainNavigator>
+        </NavigationContainer>
     </View>
   );
 }
---
+++ screens/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+
+
+export default function Cart() {
+    return <view>
+
+    </view>;
+}
